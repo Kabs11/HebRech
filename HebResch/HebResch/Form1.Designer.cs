3602b2f2fe409454cedb8de16940d60f5dc05d3e
Kabs11 <kbhingradiya8@gmail.com>
2022-05-14 11:30:24 +0200

    Add files via upload
    Initial Checkin

---
+++ HebResch/HebResch/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿
+namespace HebResch
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadTextFile = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.grdRecords = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.mainProgressBar = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecords)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnReadTextFile
+            // 
+            this.btnReadTextFile.Location = new System.Drawing.Point(195, 36);
+            this.btnReadTextFile.Name = "btnReadTextFile";
+            this.btnReadTextFile.Size = new System.Drawing.Size(75, 23);
+            this.btnReadTextFile.TabIndex = 0;
+            this.btnReadTextFile.Text = "Read";
+            this.btnReadTextFile.UseVisualStyleBackColor = true;
+            this.btnReadTextFile.Click += new System.EventHandler(this.btnReadTextFile_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(285, 36);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.mainProgressBar);
+            this.panel1.Controls.Add(this.lblFileName);
+            this.panel1.Controls.Add(this.btnOpenFile);
+            this.panel1.Controls.Add(this.btnCancel);
+            this.panel1.Controls.Add(this.btnReadTextFile);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 392);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(377, 73);
+            this.panel1.TabIndex = 2;
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(11, 40);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(57, 15);
+            this.lblFileName.TabIndex = 3;
+            this.lblFileName.Text = "FileName";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(105, 36);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(75, 23);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Select File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.grdRecords);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(377, 399);
+            this.panel2.TabIndex = 3;
+            // 
+            // grdRecords
+            // 
+            this.grdRecords.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grdRecords.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.grdRecords.Location = new System.Drawing.Point(0, 0);
+            this.grdRecords.Name = "grdRecords";
+            this.grdRecords.RowTemplate.Height = 25;
+            this.grdRecords.Size = new System.Drawing.Size(377, 399);
+            this.grdRecords.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // mainProgressBar
+            // 
+            this.mainProgressBar.ForeColor = System.Drawing.Color.LimeGreen;
+            this.mainProgressBar.Location = new System.Drawing.Point(0, 11);
+            this.mainProgressBar.Name = "mainProgressBar";
+            this.mainProgressBar.Size = new System.Drawing.Size(377, 10);
+            this.mainProgressBar.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(377, 465);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.grdRecords)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadTextFile;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView grdRecords;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.ProgressBar mainProgressBar;
+    }
+}
+
---
+++ HebResch/HebResch/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HebResch
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource cts = new CancellationTokenSource();
+        public Form1()
+        {
+            InitializeComponent();
+            mainProgressBar.Minimum = 0;
+            mainProgressBar.Maximum = 100;
+        }
+
+        private async void ReadTextFile(CancellationToken cancellationToken)
+        {
+            var dictWordsCount = new Dictionary<string, int>();
+            if (FilePath == "" || FilePath == null)
+            {
+                MessageBox.Show("Bitte wählen Sie die Datei aus!");
+                btnOpenFile_Click(null, null);
+            }
+            string[] textAll = System.IO.File.ReadAllLines(FilePath);
+            decimal intPercentageEachText = 100 / Convert.ToDecimal(textAll.Length);
+            decimal I = 0;
+            mainProgressBar.Value = 0;
+            foreach (string txt in textAll)
+            {
+                try
+                {
+                    I = I + intPercentageEachText;
+                    cancellationToken.ThrowIfCancellationRequested();
+                    string[] arrWords = txt.Split(" ");
+                    foreach (string Word in arrWords)
+                    {
+                        try
+                        {
+                            cancellationToken.ThrowIfCancellationRequested();
+                            if (dictWordsCount.ContainsKey(Word))
+                            {
+                                int intOccurence = dictWordsCount[Word];
+                                intOccurence = intOccurence + 1;
+                                dictWordsCount[Word] = intOccurence;
+                            }
+                            else
+                            {
+                                if (Word != "")
+                                {
+                                    await Task.Run(() => dictWordsCount.Add(Word, 1));
+                                }
+                            }
+                        }
+                        catch(OperationCanceledException)
+                        {
+                            break;
+                        }
+                    }
+                    mainProgressBar.Value = Convert.ToInt32(I);
+                }
+                catch(OperationCanceledException)
+                {
+                    break;
+                }
+            }
+            if (cancellationToken.IsCancellationRequested)
+            {
+                cts = new CancellationTokenSource();
+                MessageBox.Show("Der Prozess wurde storniert!");
+            }
+            else
+            {
+                DisplayRecords(dictWordsCount, cts.Token);
+            }
+            
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if(cts.IsCancellationRequested)
+            {
+                cts = new CancellationTokenSource();
+            }
+            cts.Cancel();
+        }
+
+        private void btnReadTextFile_Click(object sender, EventArgs e)
+        {
+            ReadTextFile(cts.Token);
+        }
+        private void DisplayRecords(Dictionary<string,int> WordDictionary, CancellationToken cancellationToken)
+        {
+            grdRecords.Rows.Clear();
+            grdRecords.Columns.Clear();
+            grdRecords.Columns.Add("colWord", "Wort");
+            grdRecords.Columns.Add("colOccurence", "Anzahl");
+            foreach (var keyValuePair in WordDictionary)
+            {
+                try
+                {
+                    Application.DoEvents();
+                    cancellationToken.ThrowIfCancellationRequested();
+                    DataGridViewRow row = (DataGridViewRow)grdRecords.Rows[0].Clone();
+                    row.Cells[0].Value = keyValuePair.Key;
+                    row.Cells[1].Value = keyValuePair.Value;
+                    grdRecords.Rows.Add(row);
+                }
+                catch(OperationCanceledException)
+                {
+                    MessageBox.Show("Der Prozess 'Tabelle Auflden' wurde storniert");
+                    break;
+                }
+
+            }
+        }
+
+        private string FilePath;
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Text datei auswählen";
+            openFileDialog1.DefaultExt = "txt";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FilePath = openFileDialog1.FileName;
+                lblFileName.Text = openFileDialog1.SafeFileName;
+            }
+        }
+    }
+}
